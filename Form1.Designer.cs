9a61ea5f083bc7142f37b9ea8f181f4d44e80335
Navdeep Kaur <NK030@atcltd.atc.org.nz>
2020-11-03 21:47:16 +1300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace RacingSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(392, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(377, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome To Car Race ";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Location = new System.Drawing.Point(13, 422);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1185, 188);
+            this.panel1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(347, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(742, 132);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(414, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(742, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(414, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(742, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(414, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(570, 102);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(570, 52);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(402, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Car Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(402, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "label3";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(292, 141);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(26, 114);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(41, 21);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "AI";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(26, 67);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(54, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Bob";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(26, 22);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Joe";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 646);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 49);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1055, 645);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 50);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Start Race";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(516, 644);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(202, 51);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Place Bet";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::RacingSystem.Properties.Resources.th__7_;
+            this.pictureBox4.Location = new System.Drawing.Point(12, 333);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::RacingSystem.Properties.Resources.th__6_;
+            this.pictureBox3.Location = new System.Drawing.Point(13, 255);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::RacingSystem.Properties.Resources.th__5_;
+            this.pictureBox2.Location = new System.Drawing.Point(13, 169);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RacingSystem.Properties.Resources.th__4_;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 84);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::RacingSystem.Properties.Resources.finish_line_running_track_first_to_eighth_place_visible_67324768;
+            this.pictureBox5.Location = new System.Drawing.Point(118, 84);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(1246, 299);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1376, 707);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox5);
+            this.Name = "Form1";
+            this.Text = "Cars Racing System";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RacingSystem.Classes;
+
+namespace RacingSystem
+{
+    public partial class Form1 : Form
+    {
+        Car[] Carz = new Car[4];
+        Punter[] punters = new Punter[3];
+        Car winnerCar;
+
+        Timer timer1, timer2, timer3, timer4;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            PrepareRace();//Calling method to start game
+        }
+
+        private void PrepareRace()////Initilize all data to start game
+        {
+            // Cars Info
+            Carz[0] = new Car() { CarName = "Car 1", RaceTrackLength = 830, MyPictureBox = pictureBox1 };
+            Carz[1] = new Car() { CarName = "Car 2", RaceTrackLength = 830, MyPictureBox = pictureBox2 };
+            Carz[2] = new Car() { CarName = "Car 3", RaceTrackLength = 830, MyPictureBox = pictureBox3 };
+            Carz[3] = new Car() { CarName = "Car 4", RaceTrackLength = 830, MyPictureBox = pictureBox4 };
+
+            //Punter Info
+            punters[0] = Factory.GetAPunter("Joe");
+            punters[1] = Factory.GetAPunter("Bob");
+            punters[2] = Factory.GetAPunter("AI");
+
+            punters[0].MyLabel = label3;
+            punters[0].MyRadioButton = radioButton1;
+            punters[0].MyText = textBox1;
+            punters[0].MyRadioButton.Text = punters[0].Name;
+
+
+            punters[1].MyLabel = label3;
+            punters[1].MyRadioButton = radioButton2;
+            punters[1].MyText = textBox2;
+            punters[1].MyRadioButton.Text = punters[1].Name;
+
+
+            punters[2].MyLabel = label3;
+            punters[2].MyRadioButton = radioButton3;
+            punters[2].MyText = textBox3;
+            punters[2].MyRadioButton.Text = punters[2].Name;
+
+            numericUpDown2.Minimum = 1;
+            numericUpDown2.Maximum = 4;
+            numericUpDown2.Value = 1;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            SetupBet(); //calling to place bet
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text.Contains("Game"))
+            {
+                MessageBox.Show("Game Over Thank You");
+                return;
+            }
+                timer1 = new Timer();
+            timer1.Interval = 15;
+            timer1.Tick += Cycling_Tick;
+
+            timer2 = new Timer();
+            timer2.Interval = 15;
+            timer2.Tick += Cycling_Tick;
+
+            timer3 = new Timer();
+            timer3.Interval = 15;
+            timer3.Tick += Cycling_Tick;
+
+            timer4 = new Timer();
+            timer4.Interval = 15;
+            timer4.Tick += Cycling_Tick;
+
+            timer1.Start();
+            timer2.Start();
+            timer3.Start();
+            timer4.Start();
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            int total_active = 0;
+            foreach (Punter punter in punters)
+            {
+                if (punter.Busted)
+                {
+                    //MessageBox.Show("Bet is Not Placed Because " + punter.Name + " is BUSTED");
+                }
+                else
+                {
+                    total_active++;
+                    if (punter.MyRadioButton.Checked)
+                    {
+                        if (punter.MyBet == null)
+                        {
+                            int number = (int)numericUpDown2.Value;
+                            int amount = (int)numericUpDown1.Value;
+                            bool alreadyPlaced = false;
+                            foreach (Punter pun in punters)
+                            {
+                                if (pun.MyBet != null && pun.MyBet.Car == Carz[number - 1])
+                                {
+                                    alreadyPlaced = true;
+                                    break;
+                                }
+                            }
+                            if (alreadyPlaced)
+                            {
+                                MessageBox.Show("This Car Number is Already Taken By Another Better.");
+                            }
+                            else
+                            {
+                                punter.MyBet = new Bet() { Amount = amount, Car = Carz[number - 1] };
+                            }
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("You Already Place Bet for " + punter.Name);
+                        }
+                    }
+                    if (punter.MyBet != null)
+                    {
+                        count++;
+                    }
+                }
+            }
+            SetupBet();
+
+        }
+
+        private void Cycling_Tick(object sender, EventArgs e)
+        {
+            if (sender is Timer)
+            {
+                int index = -1;
+                Timer timer = sender as Timer;
+                if (timer == timer1)
+                {
+                    index = 0;
+                }
+                else if (timer == timer2)
+                {
+                    index = 1;
+                }
+                else if (timer == timer3)
+                {
+                    index = 2;
+                }
+                else if (timer == timer4)
+                {
+                    index = 3;
+                }
+
+                if (index != -1)
+                {
+                    PictureBox pbox = Carz[index].MyPictureBox;
+                    if (pbox.Location.X + pbox.Width > Carz[index].RaceTrackLength)
+                    {
+                        if (winnerCar == null)
+                        {
+                            winnerCar = Carz[index];
+                        }
+                        timer1.Stop();
+                        timer2.Stop();
+                        timer3.Stop();
+                        timer4.Stop();
+                    }
+                    else
+                    {
+                        int jump = new Random().Next(1, 15);
+                        pbox.Location = new Point(pbox.Location.X + jump, pbox.Location.Y);
+                    }
+                }
+            }
+            if (winnerCar != null)
+            {
+                MessageBox.Show("Congratulation! " + winnerCar.CarName + " Win The Race");
+                SetupBet();
+                foreach (Punter punter in punters)
+                {
+                    if (punter.MyBet != null)
+                    {
+                        if (punter.MyBet.Car == winnerCar)
+                        {
+                            punter.Cash += punter.MyBet.Amount;
+                            punter.MyText.Text = punter.Name + " Won and now has $" + punter.Cash;
+                            punter.Winner = true;
+                        }
+                        else
+                        {
+                            punter.Cash -= punter.MyBet.Amount;
+                            if (punter.Cash == 0)
+                            {
+                                punter.MyText.Text = "BUSTED";
+                                punter.Busted = true;
+                                punter.MyRadioButton.Enabled = false;
+                            }
+                            else
+                            {
+                                punter.MyText.Text = punter.Name + " Lost and now has $" + punter.Cash;
+                            }
+                        }
+                    }
+                }
+                winnerCar = null;
+                timer1 = timer2 = timer3 = timer4 = null;
+                int count = 0;
+                foreach (Punter punter in punters)
+                {
+                    if (punter.Busted)
+                    {
+                        count++;
+                    }
+                    if (punter.MyRadioButton.Enabled && punter.MyRadioButton.Checked)
+                    {
+                        label1.Text = "Max Bet is $" + punter.Cash;
+                        numericUpDown1.Maximum = punter.Cash;
+                        numericUpDown1.Minimum = 1;
+                    }
+                    punter.MyBet = null;
+                    punter.Winner = false;
+                }
+                if (count == punters.Length)
+                {
+                    button2.Text = "Game Over";
+
+                }
+                foreach (Car Car in Carz)
+                {
+                    Car.MyPictureBox.Location = new Point(12, Car.MyPictureBox.Location.Y);
+                }
+            }
+        }
+        private void SetupBet()//place bet with this method
+        {
+            foreach (Punter punter in punters)
+            {
+                if (punter.Busted)
+                {
+                    punter.MyText.Text = "BUSTED";
+                }
+                else
+                {
+                    if (punter.MyBet == null)
+                    {
+                        punter.MyText.Text = punter.Name + " hasn't placed a bet";
+                    }
+                    else
+                    {
+                        punter.MyText.Text = punter.Name + " bets $" + punter.MyBet.Amount + " on " + punter.MyBet.Car.CarName;
+                    }
+                    if (punter.MyRadioButton.Checked)
+                    {
+                        label2.Text = "Max Bet Amount is $" + punter.Cash.ToString();
+                        button3.Text = "Place Bet for " + punter.Name;
+                        punter.MyLabel.Text = punter.Name + " Bets Amount $";
+                        numericUpDown1.Minimum = 1;
+                        numericUpDown1.Maximum = punter.Cash;
+                        numericUpDown1.Value = 1;
+                    }
+                }
+            }
+        }
+
+        
+    }
+}
